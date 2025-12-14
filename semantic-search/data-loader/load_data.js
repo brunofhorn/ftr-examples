c8d70291f274e8649bdb4d1f5a54ed775f96443f
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-12-13 22:52:10 -0300

    feat: add a movies collection

--- semantic-search/data-loader/load_data.js
+++ semantic-search/data-loader/load_data.js
@@ -1,18 +1,43 @@
 import { ChromaClient } from "chromadb";
+import csv from "csv-parser";
+import fs from "fs";
 
 const chromaClient = new ChromaClient();
-const collection = await chromaClient.getOrCreateCollection({ name: "test" });
+const collection = await chromaClient.getOrCreateCollection({ name: "movies" });
 
-await collection.add({
-  ids: ["1", "2", "3"],
-  documents: ["olá", "oi", "tudo bom"],
-  metadatas: [
-    {
-      title: "meu filme",
-    },
-    {
-      name: "Bruno",
-    },
-    {},
-  ],
-});
+const ids = [];
+const documents = [];
+const metadatas = [];
+
+fs.createReadStream("mpst_full_data.csv")
+  .pipe(csv())
+  .on("data", (row) => {
+    const document = {
+      title: row["title"],
+      tags: row["tags"],
+      synopsis: row["plot_synopsis"],
+    };
+
+    ids.push(row["imdb_id"]);
+    documents.push(JSON.stringify(document));
+    metadatas.push(document);
+  })
+  .on("end", async () => {
+    let startIdx = 0;
+
+    while (startIdx < ids.length) {
+      let endIdx = startIdx + 500;
+
+      console.log(`Adding documents from ${startIdx} to ${endIdx}`);
+
+      await collection.add({
+        ids: ids.slice(startIdx, endIdx),
+        documents: documents.slice(startIdx, endIdx),
+        metadatas: metadatas.slice(startIdx, endIdx),
+      });
+
+      startIdx = endIdx;
+    }
+
+    console.log("Done!");
+  });
