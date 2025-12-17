b8a4123d2931f35dab23a3146e119d793b53d02c
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-12-17 00:56:55 -0300

    feat(chromadb): :sparkles: add front to search movies from chromadb

--- semantic-search/front/src/App.jsx
+++ semantic-search/front/src/App.jsx
@@ -1,44 +1,49 @@
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
+
+import MovieCard from './components/MovieCard';
+import LoadingIndicator from './components/LoadingIndicator';
+import ErrorIndicator from './components/ErrorIndicator';
+import useChroma from './database/init';
+import query from './database/query';
+
+const CHROMA_COLLECTION_NAME = "movies_gemini";
 
 function App() {
+  
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [queryText, setQueryText] = useState("");
+  const [collection, isConnected] = useChroma(CHROMA_COLLECTION_NAME);
+  const [error, setError] = useState(null);
+
+  async function queryDatabase() {
+    setIsLoading(true);
+
+    try{
+      const movies = await query(collection, queryText);
+      setMovieList(movies);
+    } catch (err){
+      setError(err.toString());
+    }
+
+    setIsLoading(false);
+  }
+
   return (
     <>
       <h1>Movie Recommender</h1>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <textarea></textarea>
-        <button>Submit</button>
+      <div className="form-container">
+        <textarea onChange={e => setQueryText(e.target.value)}></textarea>
+        <button onClick={queryDatabase} disabled={!isConnected || isLoading}>Submit</button>
       </div>
-      <div class="movie-list" style={{ margin: "15px" }}>
-        <div
-          class="movie-card"
-          style={{
-            border: "1px solid lightgrey",
-            margin: "10px",
-            width: "100%",
-            height: "100px",
-          }}
-        ></div>
-        <div
-          class="movie-card"
-          style={{
-            border: "1px solid lightgrey",
-            margin: "10px",
-            width: "100%",
-            height: "100px",
-          }}
-        ></div>
-        <div
-          class="movie-card"
-          style={{
-            border: "1px solid lightgrey",
-            margin: "10px",
-            width: "100%",
-            height: "100px",
-          }}
-        ></div>
+      {isLoading ? <LoadingIndicator /> : null}
+      {error !== null ? <ErrorIndicator error={error}/> : null}
+      <div className="movie-list">
+        {movieList.map((m, idx) => <MovieCard key={idx} {...m} />)}
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
