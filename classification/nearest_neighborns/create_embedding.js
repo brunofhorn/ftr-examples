c32983ac33f01d3b2f197bc00d1c15e1018a5761
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-12-27 03:13:16 -0300

    feat(classification): :sparkles: create a nearest neighborns classification embedding cats and dogs

---
+++ classification/nearest_neighborns/create_embedding.js
@@ -0,0 +1,36 @@
+import { pipeline } from "@huggingface/transformers";
+import fs from "fs";
+import { start } from "repl";
+
+const imgEmbedder = await pipeline("image-feature-extraction", "Xenova/clip-vit-base-patch32", { dtype: "fp32" });
+
+async function embedImg(imgs){
+    return imgEmbedder(imgs, { pooling: "cls", normalize: true }).then(t => t.tolist());
+}
+
+const images = fs.readdirSync("./train").map(f => "./train/" + f);
+
+let startIdx = 0;
+
+while (startIdx < images.length) {
+    let endIdx = startIdx + 500;
+
+    console.log(`Embedding images from ${startIdx} to ${endIdx}`);
+    
+    let imgsToEmbed = images.slice(startIdx, endIdx);
+
+    const embeddings = await embedImg(imgsToEmbed);
+    const output = []
+
+    for (let i=0; i< embeddings.length; i++) {
+        output.push({
+            path: images[i + startIdx],
+            embedding: embeddings[i]
+        })
+        
+    }
+
+    fs.writeFileSync(`embeddings/embedding_${startIdx}.json`, JSON.stringify(output));
+
+    startIdx = endIdx;
+}
