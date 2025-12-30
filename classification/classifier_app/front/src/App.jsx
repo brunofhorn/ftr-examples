f9e73420cdf73cc5290f17033680e4645157fac0
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-12-30 04:03:16 -0300

    feat(classification service): :sparkles: add a backend on classification

--- classification/classifier_app/front/src/App.jsx
+++ classification/classifier_app/front/src/App.jsx
@@ -1,32 +1,24 @@
-import { useState } from 'react';
-import './App.css';
-import CaptionedImage from './components/CaptionedImage';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
 const [textInput, setTextInput] = useState(null);
-const [selectValue, setSelectValue] = useState("llm");
 const [imgSrc, setImgSrc] = useState(null);
-const [method, setMethod] = useState("llm")
 
 function classify(){
+  console.log(textInput);
   setImgSrc(textInput);
-  setMethod(selectValue);
 }
 
   return (
     <>
       <h1>Cat or Dog?</h1>
-      <input 
-      className="img-path-input" 
-      onChange={e => (setTextInput(e.target.value))} />
-      Method: <select onChange={e => setSelectValue(e.target.value)}>
-        <option value="llm">LLM</option>
-        <option value="knn">KNN</option>
-      </select>
-      <button className="classify-button" onClick={classify}>Classify!</button>
-      {
-        imgSrc !== null ? <CaptionedImage imgSrc={imgSrc} method={method}></CaptionedImage> : null
-      }
+      <input onChange={e => (setTextInput(e.target.value))} style={{width: "100%"}} />
+      <button onClick={classify}>Classify!</button>
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <img src={imgSrc} style={{height: "200px", marginTop: "20px"}} />
+        <span>Caption</span>
+      </div>
     </>
   )
 }
