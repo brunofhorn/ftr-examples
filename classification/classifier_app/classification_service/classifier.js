4c41c96968322daa1d3e909b7c4ba0ef76a674f3
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2026-01-20 23:13:45 -0300

    feat(classification): :sparkles: add a frontend and api to classification cats and dogs

--- classification/classifier_app/classification_service/classifier.js
+++ classification/classifier_app/classification_service/classifier.js
@@ -1,4 +1,43 @@
-import { classify, init } from "./knn/api.js";
+import { classify as llmClassify } from "./llm/api.js";
+import { classify as knnClassify, init as knnInit } from "./knn/api.js";
 
-init();
-console.log(await classify("https://images.theconversation.com/files/625049/original/file-20241010-15-95v3ha.jpg?ixlib=rb-4.1.0&rect=12%2C96%2C2671%2C1335&q=45&auto=format&w=668&h=324&fit=crop", 5));
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+await knnInit();
+
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.post("/classify", async (req, res) =>{
+    const method = req.body["method"];
+    const imgPath = req.body["imgPath"];
+
+    console.log(`New classification request using ${method}`);
+
+    let result = null
+    if (method == 'llm') {
+        result = await llmClassify(imgPath);
+    } else if (method == "knn"){
+        const k = req.body["k"] ? req.body["k"] : 5;
+
+        result = await knnClassify(imgPath, k);
+    }else {
+        res.status(400).send({"error": "no method found"});
+    }
+
+    res.send({category: result});
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
